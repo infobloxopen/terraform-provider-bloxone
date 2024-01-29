5e6ec4c3185e24ba51dd8fef0ffd3f45aff204e2
Ujjwal Nasra <125353741+unasra@users.noreply.github.com>
2024-01-29 23:20:26 +0530

    Updated Examples for DNS and IPAM objects (#58)
    * Added changes for resource files
    
    * Updated docs
    
    * removed configurations
    
    * Reviewing Comments
    
    ---------
    
    Co-authored-by: Venkat Venkatasubramanian <vvenkatasubramanian@infoblox.com>
    Co-authored-by: Ashish Mathew <mathewa@infoblox.com>

--- examples/resources/bloxone_ipam_address_block/resource.tf
+++ examples/resources/bloxone_ipam_address_block/resource.tf
@@ -23,4 +23,34 @@ resource "bloxone_ipam_address_block" "example_tags" {
   tags = {
     location = "site1"
   }
+  asm_config = {
+    asm_threshold       = 90
+    enable              = "true"
+    enable_notification = "true"
+    forecast_period     = 10
+    growth_factor       = 10
+    growth_type         = "percent"
+    history             = 30
+    min_total           = 2
+    min_unused          = 10
+    reenable_date       = "2024-01-24T10:10:00+00:00"
+  }
+  dhcp_config = {
+    allow_unknown = true
+    ignore_list = [
+      {
+        type  = "hardware"
+        value = "aa:bb:cc:dd:ee:ff"
+      },
+      {
+        type  = "client_text"
+        value = "001d.a18b.36d0"
+      },
+      {
+        type  = "client_hex"
+        value = "333964392D4769302F31"
+      }
+    ]
+
+  }
 }
