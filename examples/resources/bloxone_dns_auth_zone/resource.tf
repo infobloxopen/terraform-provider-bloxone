8c2886866dccd6b704bb6464ea4bd136e7efc794
unasra <unasra@infoblox.com>
2024-02-06 16:18:54 +0530

    Added ACl Support

--- examples/resources/bloxone_dns_auth_zone/resource.tf
+++ examples/resources/bloxone_dns_auth_zone/resource.tf
@@ -1,3 +1,11 @@
+resource "bloxone_keys_tsig" "test" {
+  name = "test-tsig."
+}
+
+resource "bloxone_dns_acl" "test" {
+  name = "test-acl"
+}
+
 resource "bloxone_dns_auth_zone" "example" {
   fqdn         = "example.com."
   primary_type = "cloud"
@@ -16,6 +24,17 @@ resource "bloxone_dns_auth_zone" "example" {
     {
       access  = "deny"
       element = "any"
+    },
+    {
+      element = "acl"
+      acl = bloxone_dns_acl.test.id
+    },
+    {
+      element = "tsig_key"
+      access = "deny"
+      tsig_key = {
+        key = bloxone_keys_tsig.test.id
+      }
     }
   ]
   update_acl = [
@@ -27,6 +46,17 @@ resource "bloxone_dns_auth_zone" "example" {
     {
       access  = "deny"
       element = "any"
+    },
+    {
+      element = "acl"
+      acl = bloxone_dns_acl.test.id
+    },
+    {
+      element = "tsig_key"
+      access = "deny"
+      tsig_key = {
+        key = bloxone_keys_tsig.test.id
+      }
     }
   ]
   query_acl = [
@@ -38,6 +68,17 @@ resource "bloxone_dns_auth_zone" "example" {
     {
       access  = "deny"
       element = "any"
+    },
+    {
+      element = "acl"
+      acl = bloxone_dns_acl.test.id
+    },
+    {
+      element = "tsig_key"
+      access = "deny"
+      tsig_key = {
+        key = bloxone_keys_tsig.test.id
+      }
     }
   ]
 
