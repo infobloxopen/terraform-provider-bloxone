a437f383dd51618226e4cc34c4057e45d9df82fb
Venkat Venkatasubramanian <vvenkatasubramanian@infoblox.com>
2024-02-08 18:15:57 -0800

    Model updates to support default tags in dns-data, dns-config, infra mgmt and provision objects

--- internal/service/dns_config/model_config_acl.go
+++ internal/service/dns_config/model_config_acl.go
@@ -1,113 +1,120 @@
 package dns_config
 
 import (
-	"context"
+    "context"
 
-	"github.com/hashicorp/terraform-plugin-framework/attr"
-	"github.com/hashicorp/terraform-plugin-framework/diag"
-	schema "github.com/hashicorp/terraform-plugin-framework/resource/schema"
-	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
-	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
-	"github.com/hashicorp/terraform-plugin-framework/types"
-	"github.com/hashicorp/terraform-plugin-framework/types/basetypes"
+    "github.com/hashicorp/terraform-plugin-framework/attr"
+    "github.com/hashicorp/terraform-plugin-framework/diag"
+    schema "github.com/hashicorp/terraform-plugin-framework/resource/schema"
+    "github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
+    "github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
+    "github.com/hashicorp/terraform-plugin-framework/types"
+    "github.com/hashicorp/terraform-plugin-framework/types/basetypes"
 
-	"github.com/infobloxopen/bloxone-go-client/dns_config"
+    "github.com/infobloxopen/bloxone-go-client/dns_config"
 
-	"github.com/infobloxopen/terraform-provider-bloxone/internal/flex"
+    "github.com/infobloxopen/terraform-provider-bloxone/internal/flex"
 )
 
 type ConfigACLModel struct {
-	Comment types.String `tfsdk:"comment"`
-	Id      types.String `tfsdk:"id"`
-	List    types.List   `tfsdk:"list"`
-	Name    types.String `tfsdk:"name"`
-	Tags    types.Map    `tfsdk:"tags"`
+    Comment types.String `tfsdk:"comment"`
+    Id      types.String `tfsdk:"id"`
+    List    types.List   `tfsdk:"list"`
+    Name    types.String `tfsdk:"name"`
+    Tags    types.Map    `tfsdk:"tags"`
+    TagsAll types.Map    `tfsdk:"tags_all"`
 }
 
 var ConfigACLAttrTypes = map[string]attr.Type{
-	"comment": types.StringType,
-	"id":      types.StringType,
-	"list":    types.ListType{ElemType: types.ObjectType{AttrTypes: ConfigACLItemAttrTypes}},
-	"name":    types.StringType,
-	"tags":    types.MapType{ElemType: types.StringType},
+    "comment":  types.StringType,
+    "id":       types.StringType,
+    "list":     types.ListType{ElemType: types.ObjectType{AttrTypes: ConfigACLItemAttrTypes}},
+    "name":     types.StringType,
+    "tags":     types.MapType{ElemType: types.StringType},
+    "tags_all": types.MapType{ElemType: types.StringType},
 }
 
 var ConfigACLResourceSchemaAttributes = map[string]schema.Attribute{
-	"comment": schema.StringAttribute{
-		Optional:            true,
-		MarkdownDescription: `Optional. Comment for ACL.`,
-	},
-	"id": schema.StringAttribute{
-		Computed:            true,
-		MarkdownDescription: `The resource identifier.`,
-		PlanModifiers: []planmodifier.String{
-			stringplanmodifier.UseStateForUnknown(),
-		},
-	},
-	"list": schema.ListNestedAttribute{
-		NestedObject: schema.NestedAttributeObject{
-			Attributes: ConfigACLItemResourceSchemaAttributes,
-		},
-		Optional:            true,
-		MarkdownDescription: `Optional. Ordered list of access control elements.  Elements are evaluated in order to determine access. If evaluation reaches the end of the list then access is denied.`,
-	},
-	"name": schema.StringAttribute{
-		Required:            true,
-		MarkdownDescription: `ACL object name.`,
-	},
-	"tags": schema.MapAttribute{
-		ElementType:         types.StringType,
-		Optional:            true,
-		MarkdownDescription: `Tagging specifics.`,
-	},
+    "comment": schema.StringAttribute{
+        Optional:            true,
+        MarkdownDescription: `Optional. Comment for ACL.`,
+    },
+    "id": schema.StringAttribute{
+        Computed:            true,
+        MarkdownDescription: `The resource identifier.`,
+        PlanModifiers: []planmodifier.String{
+            stringplanmodifier.UseStateForUnknown(),
+        },
+    },
+    "list": schema.ListNestedAttribute{
+        NestedObject: schema.NestedAttributeObject{
+            Attributes: ConfigACLItemResourceSchemaAttributes,
+        },
+        Optional:            true,
+        MarkdownDescription: `Optional. Ordered list of access control elements.  Elements are evaluated in order to determine access. If evaluation reaches the end of the list then access is denied.`,
+    },
+    "name": schema.StringAttribute{
+        Required:            true,
+        MarkdownDescription: `ACL object name.`,
+    },
+    "tags": schema.MapAttribute{
+        ElementType:         types.StringType,
+        Optional:            true,
+        MarkdownDescription: `Tagging specifics.`,
+    },
+    "tags_all": schema.MapAttribute{
+        ElementType:         types.StringType,
+        Computed:            true,
+        MarkdownDescription: `Tagging specifics includes the default tags.`,
+    },
 }
 
 func ExpandConfigACL(ctx context.Context, o types.Object, diags *diag.Diagnostics) *dns_config.ConfigACL {
-	if o.IsNull() || o.IsUnknown() {
-		return nil
-	}
-	var m ConfigACLModel
-	diags.Append(o.As(ctx, &m, basetypes.ObjectAsOptions{})...)
-	if diags.HasError() {
-		return nil
-	}
-	return m.Expand(ctx, diags)
+    if o.IsNull() || o.IsUnknown() {
+        return nil
+    }
+    var m ConfigACLModel
+    diags.Append(o.As(ctx, &m, basetypes.ObjectAsOptions{})...)
+    if diags.HasError() {
+        return nil
+    }
+    return m.Expand(ctx, diags)
 }
 
 func (m *ConfigACLModel) Expand(ctx context.Context, diags *diag.Diagnostics) *dns_config.ConfigACL {
-	if m == nil {
-		return nil
-	}
-	to := &dns_config.ConfigACL{
-		Comment: flex.ExpandStringPointer(m.Comment),
-		List:    flex.ExpandFrameworkListNestedBlock(ctx, m.List, diags, ExpandConfigACLItem),
-		Name:    flex.ExpandString(m.Name),
-		Tags:    flex.ExpandFrameworkMapString(ctx, m.Tags, diags),
-	}
-	return to
+    if m == nil {
+        return nil
+    }
+    to := &dns_config.ConfigACL{
+        Comment: flex.ExpandStringPointer(m.Comment),
+        List:    flex.ExpandFrameworkListNestedBlock(ctx, m.List, diags, ExpandConfigACLItem),
+        Name:    flex.ExpandString(m.Name),
+        Tags:    flex.ExpandFrameworkMapString(ctx, m.Tags, diags),
+    }
+    return to
 }
 
 func FlattenConfigACL(ctx context.Context, from *dns_config.ConfigACL, diags *diag.Diagnostics) types.Object {
-	if from == nil {
-		return types.ObjectNull(ConfigACLAttrTypes)
-	}
-	m := ConfigACLModel{}
-	m.Flatten(ctx, from, diags)
-	t, d := types.ObjectValueFrom(ctx, ConfigACLAttrTypes, m)
-	diags.Append(d...)
-	return t
+    if from == nil {
+        return types.ObjectNull(ConfigACLAttrTypes)
+    }
+    m := ConfigACLModel{}
+    m.Flatten(ctx, from, diags)
+    t, d := types.ObjectValueFrom(ctx, ConfigACLAttrTypes, m)
+    diags.Append(d...)
+    return t
 }
 
 func (m *ConfigACLModel) Flatten(ctx context.Context, from *dns_config.ConfigACL, diags *diag.Diagnostics) {
-	if from == nil {
-		return
-	}
-	if m == nil {
-		*m = ConfigACLModel{}
-	}
-	m.Comment = flex.FlattenStringPointer(from.Comment)
-	m.Id = flex.FlattenStringPointer(from.Id)
-	m.List = flex.FlattenFrameworkListNestedBlock(ctx, from.List, ConfigACLItemAttrTypes, diags, FlattenConfigACLItem)
-	m.Name = flex.FlattenString(from.Name)
-	m.Tags = flex.FlattenFrameworkMapString(ctx, from.Tags, diags)
+    if from == nil {
+        return
+    }
+    if m == nil {
+        *m = ConfigACLModel{}
+    }
+    m.Comment = flex.FlattenStringPointer(from.Comment)
+    m.Id = flex.FlattenStringPointer(from.Id)
+    m.List = flex.FlattenFrameworkListNestedBlock(ctx, from.List, ConfigACLItemAttrTypes, diags, FlattenConfigACLItem)
+    m.Name = flex.FlattenString(from.Name)
+    m.TagsAll = flex.FlattenFrameworkMapString(ctx, from.Tags, diags)
 }
--- internal/service/dns_config/model_config_auth_nsg.go
+++ internal/service/dns_config/model_config_auth_nsg.go
@@ -1,147 +1,154 @@
 package dns_config
 
 import (
-	"context"
+    "context"
 
-	"github.com/hashicorp/terraform-plugin-framework/attr"
-	"github.com/hashicorp/terraform-plugin-framework/diag"
-	schema "github.com/hashicorp/terraform-plugin-framework/resource/schema"
-	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
-	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
-	"github.com/hashicorp/terraform-plugin-framework/types"
-	"github.com/hashicorp/terraform-plugin-framework/types/basetypes"
+    "github.com/hashicorp/terraform-plugin-framework/attr"
+    "github.com/hashicorp/terraform-plugin-framework/diag"
+    schema "github.com/hashicorp/terraform-plugin-framework/resource/schema"
+    "github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
+    "github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
+    "github.com/hashicorp/terraform-plugin-framework/types"
+    "github.com/hashicorp/terraform-plugin-framework/types/basetypes"
 
-	"github.com/infobloxopen/bloxone-go-client/dns_config"
+    "github.com/infobloxopen/bloxone-go-client/dns_config"
 
-	"github.com/infobloxopen/terraform-provider-bloxone/internal/flex"
+    "github.com/infobloxopen/terraform-provider-bloxone/internal/flex"
 )
 
 type ConfigAuthNSGModel struct {
-	Comment             types.String `tfsdk:"comment"`
-	ExternalPrimaries   types.List   `tfsdk:"external_primaries"`
-	ExternalSecondaries types.List   `tfsdk:"external_secondaries"`
-	Id                  types.String `tfsdk:"id"`
-	InternalSecondaries types.List   `tfsdk:"internal_secondaries"`
-	Name                types.String `tfsdk:"name"`
-	Nsgs                types.List   `tfsdk:"nsgs"`
-	Tags                types.Map    `tfsdk:"tags"`
+    Comment             types.String `tfsdk:"comment"`
+    ExternalPrimaries   types.List   `tfsdk:"external_primaries"`
+    ExternalSecondaries types.List   `tfsdk:"external_secondaries"`
+    Id                  types.String `tfsdk:"id"`
+    InternalSecondaries types.List   `tfsdk:"internal_secondaries"`
+    Name                types.String `tfsdk:"name"`
+    Nsgs                types.List   `tfsdk:"nsgs"`
+    Tags                types.Map    `tfsdk:"tags"`
+    TagsAll             types.Map    `tfsdk:"tags_all"`
 }
 
 var ConfigAuthNSGAttrTypes = map[string]attr.Type{
-	"comment":              types.StringType,
-	"external_primaries":   types.ListType{ElemType: types.ObjectType{AttrTypes: ConfigExternalPrimaryAttrTypes}},
-	"external_secondaries": types.ListType{ElemType: types.ObjectType{AttrTypes: ConfigExternalSecondaryAttrTypes}},
-	"id":                   types.StringType,
-	"internal_secondaries": types.ListType{ElemType: types.ObjectType{AttrTypes: ConfigInternalSecondaryAttrTypes}},
-	"name":                 types.StringType,
-	"nsgs":                 types.ListType{ElemType: types.StringType},
-	"tags":                 types.MapType{ElemType: types.StringType},
+    "comment":              types.StringType,
+    "external_primaries":   types.ListType{ElemType: types.ObjectType{AttrTypes: ConfigExternalPrimaryAttrTypes}},
+    "external_secondaries": types.ListType{ElemType: types.ObjectType{AttrTypes: ConfigExternalSecondaryAttrTypes}},
+    "id":                   types.StringType,
+    "internal_secondaries": types.ListType{ElemType: types.ObjectType{AttrTypes: ConfigInternalSecondaryAttrTypes}},
+    "name":                 types.StringType,
+    "nsgs":                 types.ListType{ElemType: types.StringType},
+    "tags":                 types.MapType{ElemType: types.StringType},
+    "tags_all":             types.MapType{ElemType: types.StringType},
 }
 
 var ConfigAuthNSGResourceSchemaAttributes = map[string]schema.Attribute{
-	"comment": schema.StringAttribute{
-		Optional:            true,
-		MarkdownDescription: "Optional. Comment for the object.",
-	},
-	"external_primaries": schema.ListNestedAttribute{
-		NestedObject: schema.NestedAttributeObject{
-			Attributes: ConfigExternalPrimaryResourceSchemaAttributes,
-		},
-		Optional:            true,
-		MarkdownDescription: "Optional. DNS primaries external to BloxOne DDI. Order is not significant.",
-	},
-	"external_secondaries": schema.ListNestedAttribute{
-		NestedObject: schema.NestedAttributeObject{
-			Attributes: ConfigExternalSecondaryResourceSchemaAttributes,
-		},
-		Optional:            true,
-		MarkdownDescription: "DNS secondaries external to BloxOne DDI. Order is not significant.",
-	},
-	"id": schema.StringAttribute{
-		Computed:            true,
-		MarkdownDescription: "The resource identifier.",
-		PlanModifiers: []planmodifier.String{
-			stringplanmodifier.UseStateForUnknown(),
-		},
-	},
-	"internal_secondaries": schema.ListNestedAttribute{
-		NestedObject: schema.NestedAttributeObject{
-			Attributes: ConfigInternalSecondaryResourceSchemaAttributes,
-		},
-		Optional:            true,
-		MarkdownDescription: "Optional. BloxOne DDI hosts acting as internal secondaries. Order is not significant.",
-	},
-	"name": schema.StringAttribute{
-		Required:            true,
-		MarkdownDescription: "Name of the object.",
-		PlanModifiers: []planmodifier.String{
-			stringplanmodifier.RequiresReplaceIfConfigured(),
-		},
-	},
-	"nsgs": schema.ListAttribute{
-		ElementType:         types.StringType,
-		Optional:            true,
-		MarkdownDescription: "The resource identifier.",
-	},
-	"tags": schema.MapAttribute{
-		ElementType:         types.StringType,
-		Optional:            true,
-		MarkdownDescription: "Tagging specifics.",
-	},
+    "comment": schema.StringAttribute{
+        Optional:            true,
+        MarkdownDescription: "Optional. Comment for the object.",
+    },
+    "external_primaries": schema.ListNestedAttribute{
+        NestedObject: schema.NestedAttributeObject{
+            Attributes: ConfigExternalPrimaryResourceSchemaAttributes,
+        },
+        Optional:            true,
+        MarkdownDescription: "Optional. DNS primaries external to BloxOne DDI. Order is not significant.",
+    },
+    "external_secondaries": schema.ListNestedAttribute{
+        NestedObject: schema.NestedAttributeObject{
+            Attributes: ConfigExternalSecondaryResourceSchemaAttributes,
+        },
+        Optional:            true,
+        MarkdownDescription: "DNS secondaries external to BloxOne DDI. Order is not significant.",
+    },
+    "id": schema.StringAttribute{
+        Computed:            true,
+        MarkdownDescription: "The resource identifier.",
+        PlanModifiers: []planmodifier.String{
+            stringplanmodifier.UseStateForUnknown(),
+        },
+    },
+    "internal_secondaries": schema.ListNestedAttribute{
+        NestedObject: schema.NestedAttributeObject{
+            Attributes: ConfigInternalSecondaryResourceSchemaAttributes,
+        },
+        Optional:            true,
+        MarkdownDescription: "Optional. BloxOne DDI hosts acting as internal secondaries. Order is not significant.",
+    },
+    "name": schema.StringAttribute{
+        Required:            true,
+        MarkdownDescription: "Name of the object.",
+        PlanModifiers: []planmodifier.String{
+            stringplanmodifier.RequiresReplaceIfConfigured(),
+        },
+    },
+    "nsgs": schema.ListAttribute{
+        ElementType:         types.StringType,
+        Optional:            true,
+        MarkdownDescription: "The resource identifier.",
+    },
+    "tags": schema.MapAttribute{
+        ElementType:         types.StringType,
+        Optional:            true,
+        MarkdownDescription: "Tagging specifics.",
+    },
+    "tags_all": schema.MapAttribute{
+        ElementType:         types.StringType,
+        Computed:            true,
+        MarkdownDescription: "Tagging specifics includes default tags.",
+    },
 }
 
 func ExpandConfigAuthNSG(ctx context.Context, o types.Object, diags *diag.Diagnostics) *dns_config.ConfigAuthNSG {
-	if o.IsNull() || o.IsUnknown() {
-		return nil
-	}
-	var m ConfigAuthNSGModel
-	diags.Append(o.As(ctx, &m, basetypes.ObjectAsOptions{})...)
-	if diags.HasError() {
-		return nil
-	}
-	return m.Expand(ctx, diags)
+    if o.IsNull() || o.IsUnknown() {
+        return nil
+    }
+    var m ConfigAuthNSGModel
+    diags.Append(o.As(ctx, &m, basetypes.ObjectAsOptions{})...)
+    if diags.HasError() {
+        return nil
+    }
+    return m.Expand(ctx, diags)
 }
 
 func (m *ConfigAuthNSGModel) Expand(ctx context.Context, diags *diag.Diagnostics) *dns_config.ConfigAuthNSG {
-	if m == nil {
-		return nil
-	}
-	to := &dns_config.ConfigAuthNSG{
-		Comment:             flex.ExpandStringPointer(m.Comment),
-		ExternalPrimaries:   flex.ExpandFrameworkListNestedBlock(ctx, m.ExternalPrimaries, diags, ExpandConfigExternalPrimary),
-		ExternalSecondaries: flex.ExpandFrameworkListNestedBlock(ctx, m.ExternalSecondaries, diags, ExpandConfigExternalSecondary),
-		InternalSecondaries: flex.ExpandFrameworkListNestedBlock(ctx, m.InternalSecondaries, diags, ExpandConfigInternalSecondary),
-		Name:                flex.ExpandString(m.Name),
-		Nsgs:                flex.ExpandFrameworkListString(ctx, m.Nsgs, diags),
-		Tags:                flex.ExpandFrameworkMapString(ctx, m.Tags, diags),
-	}
-	return to
+    if m == nil {
+        return nil
+    }
+    to := &dns_config.ConfigAuthNSG{
+        Comment:             flex.ExpandStringPointer(m.Comment),
+        ExternalPrimaries:   flex.ExpandFrameworkListNestedBlock(ctx, m.ExternalPrimaries, diags, ExpandConfigExternalPrimary),
+        ExternalSecondaries: flex.ExpandFrameworkListNestedBlock(ctx, m.ExternalSecondaries, diags, ExpandConfigExternalSecondary),
+        InternalSecondaries: flex.ExpandFrameworkListNestedBlock(ctx, m.InternalSecondaries, diags, ExpandConfigInternalSecondary),
+        Name:                flex.ExpandString(m.Name),
+        Nsgs:                flex.ExpandFrameworkListString(ctx, m.Nsgs, diags),
+        Tags:                flex.ExpandFrameworkMapString(ctx, m.Tags, diags),
+    }
+    return to
 }
 
 func FlattenConfigAuthNSG(ctx context.Context, from *dns_config.ConfigAuthNSG, diags *diag.Diagnostics) types.Object {
-	if from == nil {
-		return types.ObjectNull(ConfigAuthNSGAttrTypes)
-	}
-	m := ConfigAuthNSGModel{}
-	m.Flatten(ctx, from, diags)
-	t, d := types.ObjectValueFrom(ctx, ConfigAuthNSGAttrTypes, m)
-	diags.Append(d...)
-	return t
+    if from == nil {
+        return types.ObjectNull(ConfigAuthNSGAttrTypes)
+    }
+    m := ConfigAuthNSGModel{}
+    m.Flatten(ctx, from, diags)
+    t, d := types.ObjectValueFrom(ctx, ConfigAuthNSGAttrTypes, m)
+    diags.Append(d...)
+    return t
 }
 
 func (m *ConfigAuthNSGModel) Flatten(ctx context.Context, from *dns_config.ConfigAuthNSG, diags *diag.Diagnostics) {
-	if from == nil {
-		return
-	}
-	if m == nil {
-		*m = ConfigAuthNSGModel{}
-	}
-	m.Comment = flex.FlattenStringPointer(from.Comment)
-	m.ExternalPrimaries = flex.FlattenFrameworkListNestedBlock(ctx, from.ExternalPrimaries, ConfigExternalPrimaryAttrTypes, diags, FlattenConfigExternalPrimary)
-	m.ExternalSecondaries = flex.FlattenFrameworkListNestedBlock(ctx, from.ExternalSecondaries, ConfigExternalSecondaryAttrTypes, diags, FlattenConfigExternalSecondary)
-	m.Id = flex.FlattenStringPointer(from.Id)
-	m.InternalSecondaries = flex.FlattenFrameworkListNestedBlock(ctx, from.InternalSecondaries, ConfigInternalSecondaryAttrTypes, diags, FlattenConfigInternalSecondary)
-	m.Name = flex.FlattenString(from.Name)
-	m.Nsgs = flex.FlattenFrameworkListString(ctx, from.Nsgs, diags)
-	m.Tags = flex.FlattenFrameworkMapString(ctx, from.Tags, diags)
+    if from == nil {
+        return
+    }
+    if m == nil {
+        *m = ConfigAuthNSGModel{}
+    }
+    m.Comment = flex.FlattenStringPointer(from.Comment)
+    m.ExternalPrimaries = flex.FlattenFrameworkListNestedBlock(ctx, from.ExternalPrimaries, ConfigExternalPrimaryAttrTypes, diags, FlattenConfigExternalPrimary)
+    m.ExternalSecondaries = flex.FlattenFrameworkListNestedBlock(ctx, from.ExternalSecondaries, ConfigExternalSecondaryAttrTypes, diags, FlattenConfigExternalSecondary)
+    m.Id = flex.FlattenStringPointer(from.Id)
+    m.InternalSecondaries = flex.FlattenFrameworkListNestedBlock(ctx, from.InternalSecondaries, ConfigInternalSecondaryAttrTypes, diags, FlattenConfigInternalSecondary)
+    m.Name = flex.FlattenString(from.Name)
+    m.Nsgs = flex.FlattenFrameworkListString(ctx, from.Nsgs, diags)
+    m.TagsAll = flex.FlattenFrameworkMapString(ctx, from.Tags, diags)
 }
