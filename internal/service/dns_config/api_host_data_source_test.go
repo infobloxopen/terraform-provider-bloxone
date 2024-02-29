b936da9b9f7dc90bd8bd01bee955598d764f65ac
Ashish Mathew <mathewa@infoblox.com>
2024-02-29 13:30:57 -0800

    fix unused function

--- internal/service/dns_config/api_host_data_source_test.go
+++ internal/service/dns_config/api_host_data_source_test.go
@@ -1,14 +1,10 @@
 package dns_config_test
 
 import (
-	"context"
-	"fmt"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
-	"github.com/hashicorp/terraform-plugin-testing/terraform"
 
-	"github.com/infobloxopen/bloxone-go-client/dns_config"
 	"github.com/infobloxopen/terraform-provider-bloxone/internal/acctest"
 )
 
@@ -67,27 +63,4 @@ data "bloxone_dns_hosts" "test" {
   }
 }
 `
-}
-
-func testAccCheckHostExists(ctx context.Context, resourceName string, v *dns_config.ConfigHost) resource.TestCheckFunc {
-	return func(state *terraform.State) error {
-		rs, ok := state.RootModule().Resources[resourceName]
-		if !ok {
-			return fmt.Errorf("not found: %s", resourceName)
-		}
-
-		apiRes, _, err := acctest.BloxOneClient.DNSConfigurationAPI.
-			HostAPI.
-			HostRead(ctx, rs.Primary.Attributes["results.0.id"]).
-			Execute()
-
-		if err != nil {
-			return err
-		}
-		if !apiRes.HasResult() {
-			return fmt.Errorf("expected result to be returned: %s", resourceName)
-		}
-		*v = apiRes.GetResult()
-		return nil
-	}
 }
