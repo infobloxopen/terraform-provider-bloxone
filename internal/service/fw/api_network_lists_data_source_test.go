4d315c097f66f44d45ed0c4add1d405ffe737668
unasra <unasra@infoblox.com>
2024-04-08 19:48:08 +0530

    Corrected data source UT

--- internal/service/fw/api_network_lists_data_source_test.go
+++ internal/service/fw/api_network_lists_data_source_test.go
@@ -34,33 +34,18 @@ func TestAccNetworkListsDataSource_Filters(t *testing.T) {
 	})
 }
 
-func TestAccNetworkListsDataSource_TagFilters(t *testing.T) {
-	dataSourceName := "data.bloxone_td_network_lists.test"
-	resourceName := "bloxone_td_network_list.test"
-	var v fw.AtcfwNetworkList
-	name := acctest.RandomNameWithPrefix("nl")
-
-	resource.Test(t, resource.TestCase{
-		PreCheck:                 func() { acctest.PreCheck(t) },
-		ProtoV6ProviderFactories: acctest.ProtoV6ProviderFactories,
-		CheckDestroy:             testAccCheckNetworkListsDestroy(context.Background(), &v),
-		Steps: []resource.TestStep{
-			{
-				Config: testAccNetworkListsDataSourceConfigTagFilters(name, "156.2.3.0/24", "value1"),
-				Check: resource.ComposeTestCheckFunc(
-					append([]resource.TestCheckFunc{
-						testAccCheckNetworkListsExists(context.Background(), resourceName, &v),
-					}, testAccCheckNetworkListsResourceAttrPair(resourceName, dataSourceName)...)...,
-				),
-			},
-		},
-	})
-}
-
 // below all TestAcc functions
 
 func testAccCheckNetworkListsResourceAttrPair(resourceName, dataSourceName string) []resource.TestCheckFunc {
-	return []resource.TestCheckFunc{}
+	return []resource.TestCheckFunc{
+		resource.TestCheckResourceAttrPair(resourceName, "created_time", dataSourceName, "results.0.created_time"),
+		resource.TestCheckResourceAttrPair(resourceName, "description", dataSourceName, "results.0.description"),
+		resource.TestCheckResourceAttrPair(resourceName, "items", dataSourceName, "results.0.items"),
+		resource.TestCheckResourceAttrPair(resourceName, "id", dataSourceName, "results.0.id"),
+		resource.TestCheckResourceAttrPair(resourceName, "name", dataSourceName, "results.0.name"),
+		resource.TestCheckResourceAttrPair(resourceName, "policy_id", dataSourceName, "results.0.policy_id"),
+		resource.TestCheckResourceAttrPair(resourceName, "updated_time", dataSourceName, "results.0.updated_time"),
+	}
 }
 
 func testAccNetworkListsDataSourceConfigFilters(name, item string) string {
@@ -76,22 +61,4 @@ data "bloxone_td_network_lists" "test" {
 	}
 }
 `, name, item)
-}
-
-func testAccNetworkListsDataSourceConfigTagFilters(name, item, tagValue string) string {
-	return fmt.Sprintf(`
-resource "bloxone_td_network_list" "test" {
-	name = %q
-	items = [%q]
-	tags = {
-		tag1 = %q
-	}
-}
-
-data "bloxone_td_network_lists" "test" {
-  tag_filters = {
-	tag1 = bloxone_td_network_list.test.tags.tag1
-  }
-}
-`, name, item, tagValue)
 }
