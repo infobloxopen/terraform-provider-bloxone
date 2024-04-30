d4b83a1ab15bf91dc18d76128243fe8d5838efc7
Ujjwal Nasra <unasra@infoblox.com>
2024-04-30 16:26:10 +0530

    Generated Docs and Formatted Terraform Files

--- examples/resources/bloxone_dfp_service/resource.tf
+++ examples/resources/bloxone_dfp_service/resource.tf
@@ -2,16 +2,16 @@ resource "bloxone_infra_host" "example_host" {
   display_name = "example_host"
 }
 resource "bloxone_infra_service" "example" {
-  name         = "example_dfp_service"
-  pool_id      = bloxone_infra_host.example_host.pool_id
-  service_type = "dfp"
-  desired_state = "start"
+  name           = "example_dfp_service"
+  pool_id        = bloxone_infra_host.example_host.pool_id
+  service_type   = "dfp"
+  desired_state  = "start"
   wait_for_state = false
 
 }
 
 resource "bloxone_td_internal_domain_list" "example_list" {
-  name = "example_internal_domain_list"
+  name             = "example_internal_domain_list"
   internal_domains = ["example.domain.com"]
 }
 
@@ -22,10 +22,10 @@ resource "bloxone_dfp_service" "example" {
   internal_domain_lists = [bloxone_td_internal_domain_list.example_list.id]
   resolvers_all = [
     {
-      address = "1.1.1.1"
+      address     = "1.1.1.1"
       is_fallback = true
-      is_local = false
-      protocols = ["DOT","DO53"]
+      is_local    = false
+      protocols   = ["DOT", "DO53"]
     }
   ]
 }
