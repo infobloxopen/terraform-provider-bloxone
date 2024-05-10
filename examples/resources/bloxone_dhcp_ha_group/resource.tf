ea4575eaa74ecc9cb2bdb9d41316ed56730d1ab5
Anil Gadiyar <agadiyarhj@infoblox.com>
2024-05-10 12:04:17 +0530

    Example for AnycastHA & module change inclusion for anycast,dfp

--- examples/resources/bloxone_dhcp_ha_group/resource.tf
+++ examples/resources/bloxone_dhcp_ha_group/resource.tf
@@ -10,6 +10,12 @@ data "bloxone_dhcp_hosts" "example_host_2" {
   }
 }
 
+# Get the anycast configuration for the service
+data "bloxone_anycast_configs" "example_service" {
+  service = "DNS"
+}
+
+
 resource "bloxone_dhcp_ha_group" "example_tags" {
   name = "example_ha_group_tags"
   mode = "active-active"
@@ -29,4 +35,21 @@ resource "bloxone_dhcp_ha_group" "example_tags" {
   tags = {
     location = "site1"
   }
+}
+
+resource "bloxone_dhcp_ha_group" "example_anycast" {
+  name              = "example_ha_group_anycast"
+  mode              = "anycast"
+  anycast_config_id = format("accm/ac_configs/%s", data.bloxone_anycast_configs.example_service.results.0.id)
+
+  hosts = [
+    {
+      host = data.bloxone_dhcp_hosts.example_host_1.results.0.id,
+      role = "active"
+    },
+    {
+      host = data.bloxone_dhcp_hosts.example_host_2.results.0.id,
+      role = "passive"
+    }
+  ]
 }
--- modules/bloxone_infra_host_aws/variables.tf
+++ modules/bloxone_infra_host_aws/variables.tf
@@ -47,7 +47,7 @@ variable "services" {
   description = "The services to provision on the BloxOne Host. The services must be a map of valid service type with values of \"start\" or \"stop\". Valid service types are \"dhcp\" and \"dns\"."
   type        = map(string)
   validation {
-    condition     = length(keys(var.services)) == length([for k in keys(var.services) : k if contains(["dhcp", "dns"], k)]) && alltrue([for v in values(var.services) : contains(["start", "stop"], v)])
+    condition     = length(keys(var.services)) == length([for k in keys(var.services) : k if contains(["dhcp", "dns", "anycast", "dfp"], k)]) && alltrue([for v in values(var.services) : contains(["start", "stop"], v)])
     error_message = "services must be a map of valid service type with values of start or stop"
   }
 }
