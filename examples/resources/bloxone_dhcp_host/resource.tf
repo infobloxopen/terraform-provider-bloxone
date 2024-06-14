0310e2eb5f913315a37af96c9d9e12954b825e98
Ujjwal Nasra <unasra@infoblox.com>
2024-06-14 22:38:35 +0530

    Updated Example

--- examples/resources/bloxone_dhcp_host/resource.tf
+++ examples/resources/bloxone_dhcp_host/resource.tf
@@ -11,9 +11,4 @@ resource "bloxone_dhcp_server" "dhcp_server" {
 resource "bloxone_dhcp_host" "example_dhcp_host" {
   id     = data.bloxone_infra_hosts.host.results.0.legacy_id
   server = bloxone_dhcp_server.dhcp_server.id
-
-  # Other Optional fields
-  tags = {
-    site = "Site A"
-  }
 }
