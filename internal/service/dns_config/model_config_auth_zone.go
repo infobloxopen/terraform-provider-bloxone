18c949a1b5a757ffd70299c0a7a73b90ea9293a4
Ujjwal Nasra <125353741+unasra@users.noreply.github.com>
2024-07-16 23:36:10 +0530

    Fixed NSG issue (#133)

--- internal/service/dns_config/model_config_auth_zone.go
+++ internal/service/dns_config/model_config_auth_zone.go
@@ -10,7 +10,6 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/booldefault"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/int64default"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/int64planmodifier"
-	"github.com/hashicorp/terraform-plugin-framework/resource/schema/listplanmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/objectplanmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringdefault"
@@ -196,9 +195,6 @@ var ConfigAuthZoneResourceSchemaAttributes = map[string]schema.Attribute{
 		ElementType:         types.StringType,
 		Optional:            true,
 		MarkdownDescription: `The resource identifier.`,
-		PlanModifiers: []planmodifier.List{
-			listplanmodifier.RequiresReplaceIfConfigured(),
-		},
 	},
 	"parent": schema.StringAttribute{
 		Computed:            true,
