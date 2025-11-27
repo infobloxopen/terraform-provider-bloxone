215eec08257bccd9521e333a2c65f83850d777b9
Tejashree-RS <travisuryavamshi@infoblox.com>
2025-11-27 15:31:41 +0530

    updated t.Skip message

--- internal/service/ipam/api_subnet_resource_test.go
+++ internal/service/ipam/api_subnet_resource_test.go
@@ -867,7 +867,7 @@ func TestAccSubnetResource_InheritanceSources(t *testing.T) {
 }
 
 func TestAccSubnetResource_MultipleFederatedRealms(t *testing.T) {
-	t.Skip("Skipping this test as there is no support for multiple realms on the UI")
+	t.Skip("Skipping this test as Multiple federated realms are not supported")
 	var resourceName = "bloxone_ipam_subnet.test_federated_realms"
 	var v ipam.Subnet
 	var ipSpaceName = acctest.RandomNameWithPrefix("ip-space")
